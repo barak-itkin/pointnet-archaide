5c2bfbd5f5b0d610bbc7916381cdbe18d20ca5ff
Barak Itkin <barak.itkin@gmail.com>
2018-11-02 22:22:03 +0200

    Update pointnet_cls to emit debug information

--- pointnet/models/pointnet_cls.py
+++ pointnet/models/pointnet_cls.py
@@ -15,6 +15,7 @@ def get_model_features(point_cloud, is_training, bn_decay=None, K=3,
                        reduce_max=False, name_suffix='', skip_last=False):
     """ Classification PointNet, input is BxNxK, output Bx40 """
     end_points = {}
+    debug = {}
 
     if input_transformer:
         with tf.variable_scope('transform_net1') as sc:
@@ -29,13 +30,16 @@ def get_model_features(point_cloud, is_training, bn_decay=None, K=3,
     # BxNx1x64
     net = tf_util.conv2d(input_image, 64, [1,K],
                          padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
+                         bn=False, is_training=is_training,
                          scope='conv1' + name_suffix, bn_decay=bn_decay)
+    debug['conv1'] = net
+
     # BxNx1x64
-    net = tf_util.conv2d(net, 64, [1,1],
+    net = tf_util.conv2d(net, 64, [5,1],
                          padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
+                         bn=False, is_training=is_training,
                          scope='conv2' + name_suffix, bn_decay=bn_decay)
+    debug['conv2'] = net
 
     if feature_transformer:
         with tf.variable_scope('transform_net2') as sc:
@@ -49,21 +53,24 @@ def get_model_features(point_cloud, is_training, bn_decay=None, K=3,
         net_transformed = net
 
     # BxNx1x64
-    net = tf_util.conv2d(net_transformed, 64, [1,1],
+    net = tf_util.conv2d(net_transformed, 64, [5,1],
                          padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
+                         bn=False, is_training=is_training,
                          scope='conv3' + name_suffix, bn_decay=bn_decay)
+    debug['conv3'] = net
     # BxNx1x128
-    net = tf_util.conv2d(net, 128, [1,1],
+    net = tf_util.conv2d(net, 128, [5,1],
                          padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
+                         bn=False, is_training=is_training,
                          scope='conv4' + name_suffix, bn_decay=bn_decay)
+    debug['conv4'] = net
     # BxNx1x1024
     if not skip_last:
         net = tf_util.conv2d(net, 1024, [1,1],
                              padding='VALID', stride=[1,1],
                              bn=True, is_training=is_training,
                              scope='conv5' + name_suffix, bn_decay=bn_decay)
+        debug['conv5'] = net
 
     # BxNx1024
     net = tf.squeeze(net, axis=2)
@@ -72,21 +79,24 @@ def get_model_features(point_cloud, is_training, bn_decay=None, K=3,
     if reduce_max:
         net = tf.reduce_max(net, axis=1)
 
-    return net, end_points
+    return net, end_points, debug
 
 
 def get_model_multi_features(point_cloud, names, Ks, is_training, bn_decay=None,
                              input_transformer=True, feature_transformer=True):
     vals = []  # Each value is BxNx1024
     skip = 0
+    debug = {}
     for name, K in zip(names, Ks):
-        val, _ = get_model_features(
+        val, _, debug_n = get_model_features(
             point_cloud=point_cloud[:, :, skip:(skip + K)], K=K, is_training=is_training, bn_decay=bn_decay,
             input_transformer=input_transformer, feature_transformer=feature_transformer,
             reduce_max=False, name_suffix='_' + name, skip_last=True
         )
         vals.append(val)
         skip += K
+        for k, v in debug_n.items():
+            debug[name + '_' + k] = v
 
     net = tf.concat(vals, axis=1)
     # Each value is BxNx1x...
@@ -99,19 +109,37 @@ def get_model_multi_features(point_cloud, names, Ks, is_training, bn_decay=None,
 
     net = tf.squeeze(net, axis=2)
     net = tf.reduce_max(net, axis=1)
-    return net
+    return net, debug
 
 
 def get_model_scores(model_features, is_training, n_classes, bn_decay=None):
+    debug = {}
     net = tf_util.fully_connected(model_features, 512, bn=False, is_training=is_training,
                                   scope='fc1', bn_decay=bn_decay)
     net = tf_util.dropout(net, keep_prob=0.7, is_training=is_training,
                           scope='dp1')
+    debug['fc1'] = net
     net = tf_util.fully_connected(net, 256, bn=False, is_training=is_training,
                                   scope='fc2', bn_decay=bn_decay)
     net = tf_util.dropout(net, keep_prob=0.7, is_training=is_training,
                           scope='dp2')
-    return tf_util.fully_connected(net, n_classes, activation_fn=None, scope='fc3')
+    debug['fc2'] = net
+    net = tf_util.fully_connected(net, n_classes, activation_fn=None, scope='fc3')
+    debug['fc3'] = net
+    return net, debug
+
+
+def get_model_with_multi_features(point_cloud, names, Ks, is_training, n_classes,
+                                  bn_decay=None, input_transformer=True,
+                                  feature_transformer=True):
+    features, debug = get_model_multi_features(
+        point_cloud=point_cloud, names=names, Ks=Ks, is_training=is_training,
+        bn_decay=bn_decay, input_transformer=input_transformer,
+        feature_transformer=feature_transformer
+    )
+    logits, debug2 = get_model_scores(features, is_training, n_classes, bn_decay)
+    debug.update(debug2)
+    return features, logits, debug
 
 
 def get_model(point_cloud, is_training, n_classes, bn_decay=None, K=3,
